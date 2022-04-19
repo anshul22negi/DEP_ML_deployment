2df731594064a0331c5fd80309d555a381e4e3e8
gharmanshu-7 <gharmanshu7@gmail.com>
2022-04-20 02:30:45 +0530

    removed dataset dependency from app

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from tensorflow.keras.preprocessing.image import img_to_array
 import tensorflow_hub as hub
 from os.path import dirname, abspath
 import cv2
+import json
 
 app = Flask(__name__)
 def get_model():
@@ -38,28 +39,6 @@ def load_image(img_path):
 
 IMAGE_SHAPE = (224, 224)
 BATCH_SIZE = 32
-data_dir = 'dataset'
-train_dir = os.path.join(data_dir, 'train')
-
-train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
-  rescale = 1./255,
-  rotation_range=40,
-  horizontal_flip=True,
-  width_shift_range=0.2, 
-  height_shift_range=0.2,
-  shear_range=0.2, 
-  zoom_range=0.2,
-  fill_mode='nearest' )
-
-train_generator = train_datagen.flow_from_directory(
-    train_dir, 
-    subset="training", 
-    shuffle=True, 
-    seed=42,
-    color_mode="rgb", 
-    class_mode="categorical",
-    target_size=IMAGE_SHAPE,
-    batch_size=BATCH_SIZE)
 
 def predict_class(image):
     print(image.shape)
@@ -68,7 +47,9 @@ def predict_class(image):
     
     return {classes[class_idx]: probabilities[class_idx]}
 
-classes = {j: i for i, j in train_generator.class_indices.items()}
+
+with open('classes.json') as json_file:
+    classes = json.load(json_file)
 
 def prediction(img_path):
     new_image = load_image(img_path)
